aac1ada00c3cd117ddf2a246af9d901fa67b974b
pablisco <pabs87@gmail.com>
2020-03-22 20:05:36 +0000

    Small refactor of crawler

--- crawler/src/main/kotlin/org/virustrend/crawler/Main.kt
+++ crawler/src/main/kotlin/org/virustrend/crawler/Main.kt
@@ -16,25 +16,32 @@ fun main(args: Array<String>) {
     val workingDirectory = args.firstOrNull() ?: error("missing working directory")
     val target = Paths.get(workingDirectory).resolve("../build/pages/api")
     Files.createDirectories(target)
-    timeCasesUrl.download(output = target.resolve("timeCases.pretty.json")) { parseDayInfo(prettyJson) }
-    timeCasesUrl.download(output = target.resolve("timeCases.json")) { parseDayInfo(defaultJson) }
+    val timeCasesCsv = timeCasesUrl.readCsv()
+    val dayInfoMap = timeCasesCsv.mapDayInfo()
+    dayInfoMap.toJson(prettyJson).saveTo(output = target.resolve("timeCases.pretty.json"))
+    dayInfoMap.toJson(defaultJson).saveTo(output = target.resolve("timeCases.json"))
 }
 
-private fun List<Row>.parseDayInfo(json: Json): String =
-    map {
-        it.cell("Country_Region") to DayInfo(
-            day = it.cell("Last_Update") { it.toLocalDate() },
-            confirmed = it.int("Confirmed"),
-            deaths = it.int("Deaths"),
-            recovered = it.int("Recovered"),
-            active = it.int("Active"),
-            deltaConfirmed = it.int("Delta_Confirmed"),
-            deltaRecovered = it.float("Delta_Recovered")
-        )
-    }.groupBy({ it.first }, { it.second }).let { json.stringify(DayInfoMapSerializer, it) }
+private fun Map<String, List<DayInfo>>.toJson(json: Json) =
+    json.stringify(DayInfoMapSerializer, this)
 
-private fun URL.download(output: Path, parseRows: List<Row>.() -> String) {
-    readCsv().parseRows().toByteArray().let { Files.write(output, it) }
+private fun List<Row>.mapDayInfo(): Map<String, List<DayInfo>> =
+    map {
+        it.string("Country_Region") to DayInfo(
+            day = it.localDate("Last_Update"),
+            dataPoint = DataPoint(
+                confirmed = it.int("Confirmed"),
+                deaths = it.int("Deaths"),
+                recovered = it.int("Recovered"),
+                active = it.int("Active"),
+                deltaConfirmed = it.int("Delta_Confirmed"),
+                deltaRecovered = it.float("Delta_Recovered")
+            )
+        )
+    }.groupBy({ it.first }, { it.second })
+
+private fun String.saveTo(output: Path) {
+    toByteArray().let { Files.write(output, it) }
 }
 
 private val DayInfoMapSerializer: KSerializer<Map<String, List<DayInfo>>> =
@@ -43,6 +50,11 @@ private val DayInfoMapSerializer: KSerializer<Map<String, List<DayInfo>>> =
 @Serializable
 data class DayInfo(
     @Serializable(with = LocalDateSerializer::class) val day: LocalDate,
+    val dataPoint: DataPoint
+)
+
+@Serializable
+data class DataPoint(
     val confirmed: Int,
     val deaths: Int,
     val recovered: Int,
@@ -50,7 +62,6 @@ data class DayInfo(
     val deltaConfirmed: Int,
     val deltaRecovered: Float
 )
-
 
 private const val convidRepo = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19"
 private val timeCasesUrl = URL("$convidRepo/web-data/data/cases_time.csv")
--- crawler/src/main/kotlin/org/virustrend/crawler/cvs.kt
+++ crawler/src/main/kotlin/org/virustrend/crawler/cvs.kt
@@ -4,21 +4,25 @@ import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import java.time.LocalDate
 
 internal typealias Row = Map<String, String>
 
 internal fun URL.readCsv(): List<Row> = csvReader().readAllWithHeader(get())
 
-internal fun Row.cell(key: String): String =
+internal fun Row.string(key: String): String =
     checkNotNull(this[key]) { "key \"$key\" not found" }
 
 internal fun <T : Any> Row.cell(key: String, transform: (String) -> T?): T =
-    checkNotNull(transform(cell(key))) {
-        "Failed to parse value \"${cell(key)}\" for \"$key\""
+    string(key).let { value ->
+        checkNotNull(transform(value)) {
+            "Failed to parse value \"$value\" for \"$key\""
+        }
     }
 
 internal fun Row.int(key: String): Int = cell(key) { it.toIntOrNull() }
 internal fun Row.float(key: String): Float = cell(key) { it.toFloatOrNull() }
+internal fun Row.localDate(key: String): LocalDate = cell(key) { it.toLocalDate() }
 
 private fun URL.get(): InputStream =
     with(openConnection() as HttpURLConnection) {
